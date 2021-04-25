5a2db4dbc3d5d081fb5066d62f01bd1e598fe928
Peta Jeannie <peta.jea@onja.com>
2021-04-25 15:54:39 +0300

    modified stories files

--- src/components/NextButton/NextButton.stories.js
+++ src/components/NextButton/NextButton.stories.js
@@ -5,7 +5,7 @@ import {storiesOf} from '@storybook/react-native';
 
 import NextButton from './NextButton';
 
-storiesOf('NextButton', module)
+storiesOf('Next Button', module)
   .addDecorator(story => <View>{story()}</View>)
   .add('next', () => <NextButton onPress={() => HandleClick('Next')} text="Next" disabled={false} />)
   .add('add', () => <NextButton onPress={() => HandleClick('Add')} text="Add" disabled={false} />)
